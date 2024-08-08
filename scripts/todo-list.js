d8efe5a686b3ec11cd449bf631adcbae84923da5
Matthew Davies <m.davies.work@gmail.com>
2024-08-08 12:28:47 +0900

    initial setFlag

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,3 +1,47 @@
 console.log('todo-list | Hello World!');
-console.log('todo-list | Hello World!');
-console.log('todo-list | Hello World!');
+
+/**
+ * A single ToDo in our list of Todos.
+ * @typedef {Object} ToDo
+ * @property {string} id - A unique ID to identify this todo.
+ * @property {string} label - The text of the todo.
+ * @property {boolean} isDone - Marks whether the todo is done.
+ * @property {string} userId - The user who owns this todo.
+ */
+
+
+class ToDoList {
+    static ID = 'todo-list';
+    
+    static FLAGS = {
+      TODOS: 'todos'
+    }
+    
+    static TEMPLATES = {
+      TODOLIST: `modules/${this.ID}/templates/todo-list.hbs`
+    }
+  }
+  
+  class ToDoListData {
+    static getToDosForUser(userId) {
+      return game.users.get(userId)?.getFlag(ToDoList.ID, ToDoList.FLAGS.TODO);
+    }
+
+    static createToDo(userId, toDoData) {
+      // generate a random id for this new ToDo and populate the userId
+      const newToDo = {
+        isDone: false,
+        ...toDoData,
+        id: foundry.utils.randomID(16),
+        userId,
+      }
+  
+      // construct the update to insert the new ToDo
+      const newToDos = {
+        [newToDo.id]: newToDo
+      }
+  
+      // update the database with the new ToDos
+      return game.users.get(userId)?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, newToDos);
+    }
+  }
